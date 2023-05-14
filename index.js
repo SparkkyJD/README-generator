90693069ccb43aae6cb9eba75951f6545f9ce41c
SparkkyJD <127361245+SparkkyJD@users.noreply.github.com>
2023-05-13 20:26:54 -0400

    Update index.js
    added quality comments

--- index.js
+++ index.js
@@ -53,29 +53,33 @@ const questions = [
     message: 'Please provide your Email:',
   },
 ];
-
+// init the prompt questions and user inputs.
 inquirer.prompt(questions).then((answers) => {
   const markdown = generateMarkdown(answers);
   fs.writeFile('README.md', markdown, (err) => {
     if (err) {
       console.error(err);
+      // console log when readme is added
     } else {
       console.log('README.md file created successfully!');
     }
   });
 });
-
+// generate readme.md file
 function generateMarkdown(answers) {
-  // Create a column for the table of contents
+  // to make the table of contents appear in a comumn
   const contents = answers.contents.map((item) => {
     return `- [${item}](#${item.toLowerCase().replace(/ /g, '-')})`;
   }).join('\n');
-  // Add a license badge for the selected license
+  // Adds a license badge
   let licenseBadge = "";
   if (answers.license.length > 0) {
     const licenseName = answers.license[0];
     licenseBadge = `![License: ${licenseName}](https://img.shields.io/badge/License-${licenseName.split(' ').join('%20')}-brightgreen.svg)\n\n`;
   }
+  // append user inputs to readme file. added a tags so when table of content links are clicked, 
+  // the page will take you directly to selected input.
+  // made a link that will insert the github username to a href. 
   return `# ${answers.title}\n\n${licenseBadge}
 ## Description\n${answers.description}\n\n
 ## Table of Contents\n${contents}\n\n
@@ -86,6 +90,4 @@ function generateMarkdown(answers) {
 ## Questions <a name="github"></a>\n Github Username: <a href="https://github.com/${answers.github}">${answers.github}</a>\n\nContact me via Email: ${answers.email}
 ## License type <a name="license"></a>\nThis application is covered under: ${answers.license}`;
 }
-
-
 
